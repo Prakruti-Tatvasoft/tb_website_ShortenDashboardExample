191d2625edf077d5df1c8171836316cd5f713b81
Prakruti-Tatvasoft <prakruti.ukani@tatvasoft.com>
2023-02-15 16:14:14 +0530

    added dashboard path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import React from 'react'
 import Dashboard from './pages/dashboard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Dashboard />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/d" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
