00c886bb3c3a5c7958c057729a7007bef9f9e0fa
Prakruti-Tatvasoft <prakruti.ukani@tatvasoft.com>
2023-02-15 15:54:54 +0530

    integrate endpoints

---
+++ src/pages/dashboard.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+
+const postUrl = async(baseUrl, url) => {
+	const res = await axios.post('https://3gchen2p0.g.tau.link/shorten', {
+		url,
+		base_url: baseUrl
+	})
+	return res.data
+}
+
+const Dashboard = () => {
+
+	const [url, setUrl] = useState('')
+	const [shortenUrl, setShortenUrl] = useState('')
+
+	const handleSubmit = async () => {
+		const data = await postUrl('3gchen2p0.g.tau.link', url);
+		if(data) setShortenUrl(data.url)
+		setUrl('')
+	}
+
+	const handleChange = (e) => {
+		setUrl(e.target.value);
+	}
+
+	return (
+		<div className='container'>
+			<div className='row'>
+				<div className='col-md-12'>
+					<h2 className='mb-3 mt-3'>Paste the URL to be shortened!</h2>
+					<input type="text" className='form-control' placeholder={"Enter URL here"} style={{ width: "50%" }} onChange={handleChange} value={url} />
+					<button className='btn btn-primary mb-3 mt-3' onClick={handleSubmit}>Make it short!</button>
+				</div>
+			</div>
+			{
+				shortenUrl && <h3>Shorten URL: <a href={shortenUrl}>{shortenUrl}</a></h3>
+			}
+		</div>
+	)
+}
+
+export default Dashboard
